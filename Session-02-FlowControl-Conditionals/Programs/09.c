6645534a57106d3bb2e0e40c7b59380eba55b3b2
karthik-d <karthikdesingu2000@gmail.com>
2022-05-18 19:17:15 +0530

    example programs added

---
+++ Session-02-FlowControl-Conditionals/Programs/09.c
@@ -0,0 +1,43 @@
+/**
+ * C program to print day of week using SWITCH-CASE
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int week;
+    
+    /* Input week number from user */
+    printf("Enter week number(1-7): ");
+    scanf("%d", &week);
+    
+    switch(week)
+    {
+        case 1: 
+            printf("Sunday");
+            break;
+        case 2: 
+            printf("Monday");
+            break;
+        case 3: 
+            printf("Tuesday");
+            break;
+        case 4: 
+            printf("Wednesday");
+            break;
+        case 5: 
+            printf("Thursday");
+            break;
+        case 6: 
+            printf("Friday");
+            break;
+        case 7: 
+            printf("Saturday");
+            break;
+        default: 
+            printf("Invalid input! Please enter week number between 1-7.");
+    }
+
+    return 0;
+}
